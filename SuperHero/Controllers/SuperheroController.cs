692f005a491eac69ba8090c2b402bea1f4c99d41
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-09-16 11:08:05 -0500

    I added all the view folders and started ndex method

---
+++ SuperHero/Controllers/SuperheroController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHero.Data;
+
+namespace SuperHero.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Superhero
+        public ActionResult Index()
+        {
+            var superheroTable = _context.superheros;
+            return View(superheroTable);
+        }
+
+        // GET: Superhero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superhero/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Superhero/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
